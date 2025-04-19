546e811b9c33434752eb4679a6c74e1a8e6c2c53
Matin de Wet <matindewet116@gmail.com>
2025-04-19 08:31:47 +0200

    changes Lock, and judgedQueries to make use of file level namespaces

--- BuildingBlocks/BuildingBlock.Persistence/Repositories/JudgedQueries.cs
+++ BuildingBlocks/BuildingBlock.Persistence/Repositories/JudgedQueries.cs
@@ -4,34 +4,33 @@ using BuildingBlock.Domain.Enums;
 using BuildingBlock.Persistence.Repositories.Contracts;
 using Microsoft.EntityFrameworkCore;
 
-namespace BuildingBlock.Persistence.Repositories
+namespace BuildingBlock.Persistence.Repositories;
+
+public class JudgedQueries<TCtx> : ISecureQuery where TCtx : DbContext
 {
-    public class JudgedQueries<TCtx> : ISecureQuery where TCtx : DbContext
+    private readonly TCtx _context;
+    private readonly IIdentityInfo _info;
+    private readonly IEnumerable<IProtected> _protection;
+
+    private readonly AccessRequirements _requirements;
+
+    public JudgedQueries(TCtx context, IIdentityInfo info, AccessRequirements requirements, IEnumerable<IProtected> protection)
     {
-        private readonly TCtx _context;
-        private readonly IIdentityInfo _info;
-        private readonly IEnumerable<IProtected> _protection;
+        _context = context;
+        _info = info;
+        _protection = protection;
 
-        private readonly AccessRequirements _requirements;
+        _requirements = requirements;
+    }
 
-        public JudgedQueries(TCtx context, IIdentityInfo info, AccessRequirements requirements, IEnumerable<IProtected> protection)
-        {
-            _context = context;
-            _info = info;
-            _protection = protection;
-
-            _requirements = requirements;
-        }
-
-        public IQueryable<T> Secure<T>() where T : class
-        {
-            if (_info.HasRole(ApplicationRoleEnum.SuperAdmin))
-                return _context.Set<T>();
-
-            if (_protection.FirstOrDefault(x => x.IsMatch(typeof(T))) is IProtected<T> entityLock)
-                return entityLock.Secured(_info.GetIdentityId(), _requirements.Requirement);
-
+    public IQueryable<T> Secure<T>() where T : class
+    {
+        if (_info.HasRole(ApplicationRoleEnum.SuperAdmin))
             return _context.Set<T>();
-        }
+
+        if (_protection.FirstOrDefault(x => x.IsMatch(typeof(T))) is IProtected<T> entityLock)
+            return entityLock.Secured(_info.GetIdentityId(), _requirements.Requirement);
+
+        return _context.Set<T>();
     }
 }
--- BuildingBlocks/BuildingBlock.Persistence/Repositories/Lock.cs
+++ BuildingBlocks/BuildingBlock.Persistence/Repositories/Lock.cs
@@ -2,17 +2,16 @@ using BuildingBlock.Domain.Enums;
 using BuildingBlock.Persistence.Repositories.Contracts;
 using BuildingBlock.Persistence.Repositories.Enums;
 
-namespace BuildingBlock.Persistence.Repositories
+namespace BuildingBlock.Persistence.Repositories;
+
+public abstract class Lock<T> : IProtected<T> where T : class
 {
-    public abstract class Lock<T> : IProtected<T> where T : class
+    public abstract IQueryable<T> Secured(int identityId, DataRightsEnum requirement);
+
+    public abstract Task<bool> HasAccess(T obj, int identityId, RepositoryOperationEnum operation, DataRightsEnum requirement, CancellationToken cancellationToken);
+
+    public virtual bool IsMatch(Type t)
     {
-        public abstract IQueryable<T> Secured(int identityId, DataRightsEnum requirement);
-
-        public abstract Task<bool> HasAccess(T obj, int identityId, RepositoryOperationEnum operation, DataRightsEnum requirement, CancellationToken cancellationToken);
-
-        public virtual bool IsMatch(Type t)
-        {
-            return typeof(T).IsAssignableFrom(t);
-        }
+        return typeof(T).IsAssignableFrom(t);
     }
 }
